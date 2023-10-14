451c73106ec85d69f563f578d8acb1c1facd74d7
lingeaniket <linge.aniket.10@gmail.com>
2023-10-14 10:21:05 +0530

    story css updated

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -1,139 +1,32 @@
 import React from "react";
 
+import "./stories.css";
+
 const Stories = () => {
     return (
-        <div
-            style={{
-                overflow: "visible",
-                marginBottom: "8px",
-            }}
-        >
+        <div className="story001">
             <div>
-                <div
-                    style={{
-                        padding: "8px 0",
-                        overflowY: "hidden",
-                    }}
-                >
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            height: "85px",
-                            overflowY: "hidden",
-                        }}
-                    >
-                        <div
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                                display: "flex",
-                                flexDirection: "column",
-                                position: "relative",
-                            }}
-                        >
-                            <div
-                                style={{
-                                    display: "flex",
-                                    height: "100%",
-                                    overflowX: "auto",
-                                    overflowY: "hidden",
-                                }}
-                            >
-                                <div
-                                    style={{
-                                        display: "flex",
-                                    }}
-                                >
-                                    <ul
-                                        style={{
-                                            padding: "0",
-                                            margin: "0",
-                                        }}
-                                    >
+                <div className="story002">
+                    <div className="story003">
+                        <div className="story004">
+                            <div className="story005">
+                                <div className="story006">
+                                    <ul className="story007">
                                         {/* list map stories */}
-                                        <li
-                                            style={{
-                                                verticalAlign: "baseline",
-                                                height: "100%",
-                                                padding: "0",
-                                                margin: "0",
-                                            }}
-                                        >
-                                            <div
-                                                style={{
-                                                    width: "66px",
-                                                    height: "122px",
-                                                    display: "flex",
-                                                    flexDirection: "column",
-                                                    padding: "0 4px",
-                                                    position: "relative",
-                                                    top: "2px",
-                                                }}
-                                            >
-                                                <div
-                                                    style={{
-                                                        display: "flex",
-                                                        flexDirection: "column",
-                                                        alignItems: "center",
-                                                        textAlign: "center",
-                                                        cursor: "pointer",
-                                                        width: "64px",
-                                                    }}
-                                                >
-                                                    <div
-                                                        style={{
-                                                            marginBottom: "8px",
-                                                            marginTop: "4px",
-                                                        }}
-                                                    >
-                                                        <div
-                                                            style={{
-                                                                height: "56px",
-                                                                width: "56px",
-                                                                borderRadius:
-                                                                    "50%",
-                                                                overflow:
-                                                                    "hidden",
-                                                                border: "1px solid black",
-                                                            }}
-                                                        >
+                                        <li className="story008">
+                                            <div className="story009">
+                                                <div className="story010">
+                                                    <div className="story011">
+                                                        <div className="story012">
                                                             <img
                                                                 src=""
                                                                 alt=""
-                                                                style={{
-                                                                    height: "100%",
-                                                                    width: "100%",
-                                                                    verticalAlign:
-                                                                        "middle",
-                                                                    maxWidth:
-                                                                        "100%",
-                                                                }}
+                                                                className="story013"
                                                             />
                                                         </div>
                                                     </div>
-                                                    <div
-                                                        style={{
-                                                            color: "rgb(115,115,115)",
-                                                            fontWeight: 400,
-                                                            fontSize: "12px",
-                                                            lineHeight: "12px",
-                                                        }}
-                                                    >
-                                                        <div
-                                                            style={{
-                                                                whiteSpace:
-                                                                    "nowrap",
-                                                                maxWidth:
-                                                                    "74px",
-                                                                padding:
-                                                                    "0 2px",
-                                                                overflow:
-                                                                    "hidden",
-                                                                textAlign:
-                                                                    "center",
-                                                            }}
-                                                        >
+                                                    <div className="story014">
+                                                        <div className="story015">
                                                             username
                                                         </div>
                                                     </div>
