cfc3773b5caadfd84961efdf0791f31c6bd1b132
Aniket Linge <119785335+lingeaniket@users.noreply.github.com>
2023-10-24 10:38:01 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import Sidebar from "../Sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
+    console.log("Home Component Rendered");
     return (
         <div className="grid01">
             <Sidebar />
