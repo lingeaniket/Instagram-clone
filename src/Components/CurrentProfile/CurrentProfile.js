78ec9dc21e8497551c83343b19513ea8decb085a
Aniket Linge <119785335+lingeaniket@users.noreply.github.com>
2023-10-27 14:06:18 +0530

    Update CurrentProfile.js

--- src/Components/CurrentProfile/CurrentProfile.js
+++ src/Components/CurrentProfile/CurrentProfile.js
@@ -2,6 +2,7 @@ import React from "react";
 import Profile from "../Profile/Profile";
 
 const CurrentProfile = () => {
+    console.log("Reloaded");
     const userId = JSON.parse(localStorage.getItem("userId"));
 
     return <Profile id={userId} module="currentProfile" />;
