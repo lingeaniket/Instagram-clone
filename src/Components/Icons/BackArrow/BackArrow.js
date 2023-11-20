037d47b7113b3e752b41d722277f1145e514d46c
lingeaniket <linge.aniket.10@gmail.com>
2023-11-20 16:12:41 +0530

    icon added

---
+++ src/Components/Icons/BackArrow/BackArrow.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const BackArrow = () => {
+    return (
+        <svg aria-label="Back" class="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24">
+            <title>Back</title>
+            <line
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                x1="2.909"
+                x2="22.001"
+                y1="12.004"
+                y2="12.004"
+            ></line>
+            <polyline
+                fill="none"
+                points="9.276 4.726 2.001 12.004 9.276 19.274"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+            ></polyline>
+        </svg>
+    );
+};
+
+export default BackArrow;
