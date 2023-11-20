1932cf2f45f8a15fb318b8e1028d40e7e9792601
lingeaniket <linge.aniket.10@gmail.com>
2023-11-20 16:13:09 +0530

    3 components added

---
+++ src/Components/NewPost/Image.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useEffect, memo } from "react";
+
+const Image = ({ id }) => {
+    useEffect(() => {
+        const getImage = () => {
+            axios.get(`https://picsum.photos/id/${id}/500/500`).then((res) => {});
+        };
+    }, []);
+    return (
+        <div
+            style={{
+                aspectRatio: "1/1",
+                cursor: 'pointer'
+            }}
+        >
+            <img
+                loading="lazy"
+                src={`https://picsum.photos/id/${id}/500/500`}
+                style={{
+                    maxWidth: "100%",
+                    verticalAlign: "middle",
+                }}
+                alt=""
+                id={id}
+            />
+        </div>
+    );
+};
+
+export default memo(Image);
---
+++ src/Components/NewPost/ImageComponent/ImageComponent.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import Image from "../Image";
+import "./imageComponent.css";
+import { useDispatch } from "react-redux";
+import { changeImageid } from "../../../Features/newPostDetailsSlice";
+
+const ImageComponent = ({handleStep}) => {
+    const [photos, setPhotos] = useState([]);
+    const [selectedPhoto, setSelectedPhoto] = useState();
+    const [page, setPage] = useState(0);
+    const dispatch = useDispatch();
+
+    const handlePhoto = (e) => {
+        setSelectedPhoto(Number(e.target.id));
+    };
+
+    const handleSelectedPhoto = () => {
+        dispatch(changeImageid(selectedPhoto));
+        handleStep(3);
+
+    };
+
+    const handlePrev = () => {
+        setSelectedPhoto(null);
+        setPage((prev) => prev - 1);
+    };
+    const handleNext = () => {
+        setSelectedPhoto("");
+        setPage((prev) => prev + 1);
+    };
+    useEffect(() => {
+        // alert("runned")
+        const getPhotoId = async () => {
+            setPhotos(() => {
+                return [];
+            });
+            for (let i = page * 10; i < page * 10 + 9; i++) {
+                setPhotos((prev) => [...prev, i]);
+            }
+        };
+        getPhotoId();
+    }, [page]);
+    return (
+        <div className="img_post001">
+            <div className="img_post002">
+                {photos.map((val) => (
+                    <div
+                        style={{
+                            border: `2px solid ${selectedPhoto === val ? "blue" : "white"}`,
+                            width: "33%",
+                            margin: "2px 0",
+                            position: "relative",
+                        }}
+                        onClick={handlePhoto}
+                        key={val}
+                    >
+                        <Image id={val} />
+                        {selectedPhoto === val && (
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    top: 0,
+                                    bottom: 0,
+                                    height: "100%",
+                                    width: "100%",
+                                    backgroundColor: "rgb(255 255 255 / 56%)",
+                                }}
+                            ></div>
+                        )}
+                    </div>
+                ))}
+            </div>
+            <div className="img_post003">
+                <div>
+                    <button
+                        className={`nav_buttons ${page === 0 ? "nav_buttons_dis" : "nav_buttons_act"}`}
+                        disabled={page === 0}
+                        onClick={handlePrev}
+                    >
+                        Prev
+                    </button>
+                </div>
+                {selectedPhoto || selectedPhoto === 0 ? (
+                    <div>
+                        <button
+                            className="nav_buttons"
+                            style={{
+                                backgroundColor: "#179500",
+                                cursor: "pointer",
+                            }}
+                            onClick={handleSelectedPhoto}
+                        >
+                            Continue to post
+                        </button>
+                    </div>
+                ) : null}
+                <div>
+                    <button className="nav_buttons nav_buttons_act" onClick={handleNext}>
+                        Next
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImageComponent;
