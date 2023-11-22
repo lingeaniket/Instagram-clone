5cdcb3c6360c2ce981d7cf57c6731337ff4a7cb8
lingeaniket <linge.aniket.10@gmail.com>
2023-11-22 13:21:24 +0530

    ViewPostComponent updated

---
+++ src/Components/ViewPostComponent/ViewPostComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import FullPost from "../Fullpost/FullPost";
+import "./viewPostComponent.css";
+import ProfileScroll from "../ProfileScroll/ProfileScroll";
+
+const ViewPostComponent = () => {
+    return (
+        <div>
+            <div className="appSwitch01">
+                <FullPost />
+            </div>
+            <div className="appSwitch02">
+                <ProfileScroll />
+            </div>
+        </div>
+    );
+};
+
+export default ViewPostComponent;
