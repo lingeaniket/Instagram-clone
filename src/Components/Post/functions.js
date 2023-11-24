681d18efc7629ca95aff59ef407108da8a42314e
lingeaniket <linge.aniket.10@gmail.com>
2023-11-24 13:50:34 +0530

    post function updated

--- src/Components/Post/functions.js
+++ src/Components/Post/functions.js
@@ -97,3 +97,12 @@ const loadPosts = async (id) => {
         console.log(err);
     }
 };
+
+export const arrayFill = (arr) => {
+    if (arr.length === 1) {
+        return [...arr, {}, {}];
+    } else if (arr.length === 2) {
+        return [...arr, {}];
+    }
+    return arr;
+};
