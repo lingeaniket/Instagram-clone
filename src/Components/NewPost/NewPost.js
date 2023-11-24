1e37af2f7201b0f7895b714dc623f12c63fb0ad7
lingeaniket <linge.aniket.10@gmail.com>
2023-11-24 12:55:49 +0530

    newPost component updated

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -2,16 +2,9 @@ import React, { useCallback, useRef, useState } from "react";
 import Modal from "../Modal/Modal";
 
 import "./newPost.css";
-import "../Fullpost/fullPost.css"
-import ImageComponent from "./ImageComponent/ImageComponent";
-import ViewComponent from "./ViewImageComponent/ViewComponent";
-import BackArrow from "../Icons/BackArrow/BackArrow";
-import Posting from "./Posting/Posting";
-import { useDispatch } from "react-redux";
-import { sharePost } from "../../Features/newPostDetailsSlice";
-import PostSuccess from "./PostSuccess/PostSuccess";
-import PostSuccess2 from "./PostSuccess/PostSuccess2";
-import LandPage from "./LandPage/LandPage";
+import "../Fullpost/fullPost.css";
+
+import PostFolder from "./PostFolder/PostFolder";
 
 const NewPost = ({ setOpen }) => {
     const handleModalClose = (e) => {
@@ -33,15 +26,6 @@ const NewPost = ({ setOpen }) => {
 
     const modalref = useRef();
 
-    const dispatch = useDispatch();
-
-    const handlePosting = () => {
-        dispatch(sharePost());
-        setTimeout(() => {
-            setStep(6);
-        }, 3000);
-    };
-
     const handleStep = useCallback((step) => {
         setStep(step);
     }, []);
@@ -62,67 +46,7 @@ const NewPost = ({ setOpen }) => {
                                         width: `${step !== 4 ? (step === 5 || step === 6 ? "448px" : "535px") : "875px"}`, //875
                                     }}
                                 >
-                                    <div className="new_post007">
-                                        <div className="w_100">
-                                            <div className="new_post008">
-                                                {![1, 2, 5, 6].includes(step) && (
-                                                    <div
-                                                        style={{
-                                                            cursor: "pointer",
-                                                            flexGrow: 1,
-                                                        }}
-                                                        onClick={() => {
-                                                            handleStep(step - 1);
-                                                        }}
-                                                    >
-                                                        <BackArrow />
-                                                    </div>
-                                                )}
-                                                <h1 className="new_post009">
-                                                    <div>{steps[step - 1]}</div>
-                                                </h1>
-                                                {![1, 2, 5, 6].includes(step) && (
-                                                    <div
-                                                        style={{
-                                                            fontWeight: 600,
-                                                            fontSize: "16px",
-                                                            color: "rgb(0,149,246)",
-                                                            cursor: "pointer",
-                                                            flexGrow: 1,
-                                                            textAlign: "end",
-                                                        }}
-                                                        onClick={() => {
-                                                            if (step === 4) {
-                                                                handlePosting();
-                                                            }
-                                                            handleStep(step + 1);
-                                                        }}
-                                                    >
-                                                        {step === 4 ? "Share" : "Next"}
-                                                    </div>
-                                                )}
-                                            </div>
-                                        </div>
-                                        {step === 4 ? (
-                                            <Posting />
-                                        ) : step !== 5 && step !== 6 ? (
-                                            <div className="new_post010">
-                                                {step === 1 && <LandPage handleStep={handleStep} />}
-                                                {step === 2 && <ImageComponent handleStep={handleStep} />}
-                                                {step === 3 && <ViewComponent />}
-                                            </div>
-                                        ) : (
-                                            <div
-                                                className="new_post010"
-                                                style={{
-                                                    width: "448px",
-                                                }}
-                                            >
-                                                {step === 5 && <PostSuccess />}
-                                                {step === 6 && <PostSuccess2 />}
-                                            </div>
-                                        )}
-                                    </div>
+                                    <PostFolder step={step} handleStep={handleStep} steps={steps} />
                                 </div>
                             </div>
                         </div>
