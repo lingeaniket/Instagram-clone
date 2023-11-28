969deac4849ee019738a98507665c728c7486121
lingeaniket <linge.aniket.10@gmail.com>
2023-11-28 16:44:17 +0530

    Home.js updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,14 +3,25 @@ import { Outlet } from "react-router-dom";
 
 import Sidebar from "../Sidebar/Sidebar";
 
+// 1263 - side bar length to 72 , 1159 - right bar hide, 768px mobile view side bar to bottom and mobile nav should appear
+
+import "./home.css";
 const Home = () => {
     return (
-        <div className="grid01">
-            <Sidebar />
-            <div className="span01">
-                <Suspense fallback={<></>}>
-                    <Outlet />
-                </Suspense>
+        <div className="home_01">
+            <div className="home_02">
+                <div className="home_03">
+                    <Sidebar />
+                </div>
+                <div className="home_04">
+                    <div className="home_05">
+                        <div className="home_06">
+                            <Suspense fallback={<></>}>
+                                <Outlet />
+                            </Suspense>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
