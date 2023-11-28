5899adce6d7b57c9f9fa4ff7d52d88a703b5aa7f
lingeaniket <linge.aniket.10@gmail.com>
2023-11-28 16:44:44 +0530

    Rightside bar updated

--- src/Components/RightSideBar/RightSideBar.js
+++ src/Components/RightSideBar/RightSideBar.js
@@ -7,52 +7,50 @@ import "./rightSideBar.css";
 
 const RightSideBar = () => {
     return (
-        <div className="rsb001">
-            <div className="rsb002">
-                <List loading={false} user={{ username: "abc", avatar: 1 }} type="timeline" userAcc={true} modalRequired={true} />
-                <div className="rsb003">
-                    <div className="rsb004">
-                        <div className="rsb005">
-                            <div className="rsb006">
-                                <div className="rsb007">Suggested for you</div>
-                                <div>
-                                    <span className="rsb008">See All</span>
-                                </div>
+        <div className="rsb002">
+            <List loading={false} user={{ username: "abc", avatar: 1 }} type="timeline" userAcc={true} modalRequired={true} />
+            <div className="rsb003">
+                <div className="rsb004">
+                    <div className="rsb005">
+                        <div className="rsb006">
+                            <div className="rsb007">Suggested for you</div>
+                            <div>
+                                <span className="rsb008">See All</span>
                             </div>
-                            <div className="rsb009">
-                                <div className="rsb010">
-                                    <div className="rsb011">
-                                        <div className="rsb012">
-                                            {/* acount list all products */}
-                                            <div className="rsb013">
-                                                <List
-                                                    user={{
-                                                        username: "aniket",
-                                                        avatar: 12,
-                                                    }}
-                                                    index={1}
-                                                    type="timeline"
-                                                    modalRequired={true}
-                                                />
-                                                <List
-                                                    index={2}
-                                                    user={{
-                                                        username: "aniket",
-                                                        avatar: 13,
-                                                    }}
-                                                    modalRequired={true}
-                                                    type="timeline"
-                                                />
-                                                <List
-                                                    index={3}
-                                                    modalRequired={true}
-                                                    user={{
-                                                        username: "aniket",
-                                                        avatar: 14,
-                                                    }}
-                                                    type="timeline"
-                                                />
-                                            </div>
+                        </div>
+                        <div className="rsb009">
+                            <div className="rsb010">
+                                <div className="rsb011">
+                                    <div className="rsb012">
+                                        {/* acount list all products */}
+                                        <div className="rsb013">
+                                            <List
+                                                user={{
+                                                    username: "aniket",
+                                                    avatar: 12,
+                                                }}
+                                                index={1}
+                                                type="timeline"
+                                                modalRequired={true}
+                                            />
+                                            <List
+                                                index={2}
+                                                user={{
+                                                    username: "aniket",
+                                                    avatar: 13,
+                                                }}
+                                                modalRequired={true}
+                                                type="timeline"
+                                            />
+                                            <List
+                                                index={3}
+                                                modalRequired={true}
+                                                user={{
+                                                    username: "aniket",
+                                                    avatar: 14,
+                                                }}
+                                                type="timeline"
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -60,8 +58,8 @@ const RightSideBar = () => {
                         </div>
                     </div>
                 </div>
-                <Copyright />
             </div>
+            <Copyright />
         </div>
     );
 };
