68422dcc4c5c8d14e93b527ef247b2ef8b3c8850
lingeaniket <linge.aniket.10@gmail.com>
2023-11-29 10:14:10 +0530

    Sidebar updated

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Suspense, lazy, useEffect, useRef, useState, memo } from "react";
 
-
 import { socket } from "../../App";
 import { tabArr } from "../ExtraData/extraData";
 
@@ -51,68 +50,68 @@ const Sidebar = () => {
     }, []);
 
     return (
-                <div className="side05">
-                    <div
-                        className="side03"
-                        onClick={() => {
-                            navigate("/");
-                        }}
-                    >
-                        <div className="side08">
-                            <div className="side09">
-                                <span className="side06">
-                                    <InstagramTitle />
-                                </span>
-                                <span className="side07">
-                                    <Instagram />
-                                </span>
-                            </div>
-                        </div>
+        <div className="side05">
+            <div
+                className="side03"
+                onClick={() => {
+                    navigate("/");
+                }}
+            >
+                <div className="side08">
+                    <div className="side09">
+                        <span className="side06">
+                            <InstagramTitle />
+                        </span>
+                        <span className="side07">
+                            <Instagram />
+                        </span>
                     </div>
-                    <div className="side04">
-                        {tabArr.map((tab, i) => {
-                            if (tab.title === "profile") {
-                                if (userId) {
-                                    return <Tab id={id} key={i} tab={tab} searchOpen={handleCollapse} searchRef={searchRef} />;
-                                } else {
-                                    return <></>;
-                                }
-                            } else if (tab.title === "create") {
-                                return (
-                                    <div key={i} className="tab01" onClick={handleCreate}>
-                                        <div className="tab02">{tab.icon}</div>
-                                        <div className="tab03">{tab.title}</div>
-                                    </div>
-                                );
-                            }
-                            return <Tab id={id} key={i} tab={tab} searchOpen={handleCollapse} searchRef={searchRef} />;
-                        })}
-                    </div>
-                    <div className="w_100">
-                        <Tab
-                            tab={{
-                                title: "More",
-                                icon: <MoreSettings />,
-                            }}
-                            searchRef={searchRef}
-                        />
-                    </div>
-                    {searchDiv && <SearchComponent searchDiv={searchDiv} searchCloseFunc={handleSearch} searchRef={searchRef} />}
-                    {createOpen && (
-                        <div>
-                            <span className="switchView01">
-                                <Suspense fallback={<></>}>
-                                    <NewPost setOpen={setCreateOpen} />
-                                </Suspense>
-                            </span>
-                            <span className="switchView02">
-                                <Suspense fallback={<></>}>
-                                    <NewPostMobile />
-                                </Suspense>
-                            </span>
-                        </div>
-                    )}
                 </div>
+            </div>
+            <div className="side04">
+                {tabArr.map((tab, i) => {
+                    if (tab.title === "profile") {
+                        if (userId) {
+                            return <Tab id={id} key={i} tab={tab} searchOpen={handleCollapse} searchRef={searchRef} />;
+                        } else {
+                            return <></>;
+                        }
+                    } else if (tab.title === "create") {
+                        return (
+                            <div key={i} className="tab01" onClick={handleCreate}>
+                                <div className="tab02">{tab.icon}</div>
+                                <div className="tab03">{tab.title}</div>
+                            </div>
+                        );
+                    }
+                    return <Tab id={id} key={i} tab={tab} searchOpen={handleCollapse} searchRef={searchRef} />;
+                })}
+            </div>
+            <div className="w_100">
+                <Tab
+                    tab={{
+                        title: "More",
+                        icon: <MoreSettings />,
+                    }}
+                    searchRef={searchRef}
+                />
+            </div>
+            {searchDiv && <SearchComponent searchDiv={searchDiv} searchCloseFunc={handleSearch} searchRef={searchRef} />}
+            {createOpen && (
+                <div>
+                    <span className="switchView01">
+                        <Suspense fallback={<></>}>
+                            <NewPost setOpen={setCreateOpen} />
+                        </Suspense>
+                    </span>
+                    <span className="switchView02">
+                        <Suspense fallback={<></>}>
+                            <NewPostMobile />
+                        </Suspense>
+                    </span>
+                </div>
+            )}
+        </div>
         // <div className="side01">
         //     <div className="side02">
         //     </div>
